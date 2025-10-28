6c772334489d2f9858f700d2486abaf130edbb48
christian <ronquechristian@gmail.com>
2025-10-29 05:36:10 +0800

    cors

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
-
 connectDB();
+
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [process.env.CLIENT_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,6 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
