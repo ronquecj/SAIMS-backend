9e72e0a1335b314e7e28ea382a89cb3c62420de5
ronquecj <ronquechristian@gmail.com>
2026-03-18 20:14:40 +0800

    ng+2

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const corsOptions = {
   },
 };
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
